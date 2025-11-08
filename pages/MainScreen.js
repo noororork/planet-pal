8d9339d03416240425d0cf7603cc29568b8d0d27
aidahpvz <aidahparvez@gmail.com>
2025-11-08 21:58:34 +0000

    Changes based on health

--- pages/MainScreen.js
+++ pages/MainScreen.js
@@ -2,14 +2,23 @@ import React, { useState, useEffect } from "react";
 import { ImageBackground, StyleSheet, View, Text, Button } from "react-native";
 
 export default function MainScreen() {
+    const [health, setHealth] = useState(100);
+    const [backgroundImage, setBackgroundImage] = useState(require("../resources/9.png"));
+
+    const updateHealthImage = (currentHealth) => {
+        if (currentHealth >= 80) return require("../resources/9.png");
+        else if (currentHealth >= 60) return require("../resources/7.png");
+        else if (currentHealth >= 40) return require("../resources/5.png");
+        else if (currentHealth >= 20) return require("../resources/3.png");
+        else return require("../resources/1.png");
+    };
+
+    useEffect(() => {
+        setBackgroundImage(updateHealthImage(health));
+    }, [health]);
+
     return (
-        <ImageBackground
-            source = {require("../resources/5.png")}
-            style = {styles.background}
-            resizeMode = "cover"
-        >
-        </ImageBackground>
-    );
+        <ImageBackground source = {backgroundImage} style = {styles.background} resizeMode = "cover"></ImageBackground>);
 }
 
 const styles = StyleSheet.create({
